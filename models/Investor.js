5c57638609f1335f85d53df8f53c3c2e76641dfa
Lucky Wagh <waghlucky63@gmail.com>
2025-12-12 12:47:02 +0530

    Update Investor.js

--- models/Investor.js
+++ models/Investor.js
@@ -3,16 +3,23 @@ import mongoose from "mongoose";
 const InvestorSchema = new mongoose.Schema(
   {
     firebaseUID: { type: String, required: true, unique: true },
+
+    // Basic
     name: String,
     email: String,
 
+    // Investor Details
     firmName: String,
-    investmentSize: String,
     investorType: String,
+
+    investmentSize: String,     // e.g. 10L - 50L
+    investmentRange: String,    // NEW
+    sectorInterest: String,     // NEW
+    website: String,            // NEW
 
     profileCompleted: { type: Boolean, default: false },
 
-    userType: { type: String, default: "investor" },  // ⬅ added
+    userType: { type: String, default: "investor" },
   },
   { timestamps: true }
 );
